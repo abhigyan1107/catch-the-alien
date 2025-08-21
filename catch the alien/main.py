a832886bf73fe2ee63f382b8655fae7f143e2513
abhigyan1107 <Abhigyanpatni11@gmail.com>
2025-08-21 18:28:34 +0530

    Update main.py

--- catch the alien/main.py
+++ catch the alien/main.py
@@ -4,19 +4,33 @@ import random
 #setting width and height of the screen
 WIDTH = 700
 HEIGHT = 425
+
+message = "Catch The Alien"
+
+
 #creationg the game charecter ghost
-ghost = Actor("ghost")
-ghost.pos = (500,333)
+alien = Actor("ghost")
+alien.pos = (500,333)
 
-def move_ghost():
-    ghost.pos = (random.randint(50,650),random.randint(50,375))
+def move_alien():
+    alien.pos = (random.randint(50,650),random.randint(50,375))
 
-#creating default functions to be used or drawing on the screen
+#adding mouse clicking events 
+def on_mouse_down(pos):
+    global message
+    if alien.collidepoint(pos):
+        move_alien()
+        message = "Noo, you caught me"
+    else:
+        message = "Haha, you'll never catch me"
+
+#creating default system functions to be used or drawing on the screen
 def draw():
     #changing the screen color
     screen.fill("red")
     #displaying ghost actor
-    ghost.draw()
+    alien.draw()
+    screen.draw.text(message,center = alien.pos,fontsize = 20)
 
 #comMAND TO KEEP UR SCREEN VISABLE AND STOP IT FROM SCROLLING
 pgzrun.go()
